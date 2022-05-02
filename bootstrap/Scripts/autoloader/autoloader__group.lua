5f7db51e7cf0aea3241e5a24dc6177ca743b982c
Megophrys Nasuta <nasutater@gmail.com>
2022-05-01 19:44:23 -0500

    Update bootstrap to autoload libraries

--- bootstrap/Scripts/autoloader/autoloader__group.lua
+++ bootstrap/Scripts/autoloader/autoloader__group.lua
@@ -5,7 +5,7 @@ function settingsReloaded(_, path)
     if __settingsReloadTimer__ then killTimer(__settingsReloadTimer__) end
     removeFileWatch(settings)
     __settingsReloadTimer__ = tempTimer(0.25, function()
-      cecho('\n<cyan>SETTINGS RELOADED WITH LATEST CHANGES\n')
+      cecho('\n<cyan>MAIN SETTINGS RELOADED WITH LATEST CHANGES\n')
       installPackage(settings)
       addFileWatch(settings)
     end)
@@ -15,3 +15,22 @@ end
 cecho('\n<cyan>Watching settings file for changes...\n')
 addFileWatch(settings)
 registerAnonymousEventHandler('sysPathChanged', settingsReloaded)
+
+
+local libraries = getMudletHomeDir() .. '/libraries.xml'
+__librariesReloadTimer__ = nil
+function librariesReloaded(_, path)
+  if path == libraries then
+    if __librariesReloadTimer__ then killTimer(__librariesReloadTimer__) end
+    removeFileWatch(libraries)
+    __librariesReloadTimer__ = tempTimer(0.25, function()
+      cecho('\n<cyan>SECONDARY LIBRARIES RELOADED WITH LATEST CHANGES\n')
+      installPackage(libraries)
+      addFileWatch(libraries)
+    end)
+  end
+end
+
+cecho('\n<cyan>Watching libraries file for changes...\n')
+addFileWatch(libraries)
+registerAnonymousEventHandler('sysPathChanged', librariesReloaded)
